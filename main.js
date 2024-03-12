47626de539eb7bacebef6698d14f8f057c5bbc9d
NidaWorld <160415456+nidakhan6@users.noreply.github.com>
2024-03-12 21:23:09 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+"use strict";
+//class4
+//addition
+let num1 = 10;
+let num2 = 5;
+let sum = (num1 + num2);
+console.log(sum);
+//subtraction
+let a = 15;
+let b = 3;
+let difference = (a - b);
+console.log(difference);
+//multiplication
+let c = 6;
+let d = 6;
+let number = (c * d);
+console.log(number);
+//division
+let students = 20;
+let pencils = 10;
+let div = (students / pencils);
+console.log(div);
+//exponentiation
+let ab = 4;
+let cd = 3;
+let exp = (ab ** cd);
+console.log(exp);
+//modules
+let toffee = 5;
+let child = 3;
+let mod = (toffee % child);
+console.log(mod);
---
+++ main.ts
@@ -0,0 +1,38 @@
+//class4
+
+//addition
+let num1 : number = 10
+let num2: number = 5
+let sum = (num1+num2);
+console.log(sum);
+
+//subtraction
+let a: number = 15
+let b: number=3
+let difference = (a-b)
+console.log(difference);
+
+
+//multiplication
+let c : number=6
+let d :number =6
+let number= (c * d);
+console.log(number);
+
+//division
+let students: number =20
+let pencils :number= 10
+let div = (students / pencils)
+console.log (div)
+
+//exponentiation
+let ab : number = 4
+let cd : number = 3
+let exp=(ab**cd)
+console.log(exp);
+
+//modules
+let toffee: number = 5
+let child :number =3
+let mod= (toffee% child);
+console.log(mod);
